72b0d5698f180ab2360fbdb96dda2e6c7909b66f
jenikamban <nenijeni275@gmail.com>
2026-04-04 03:45:34 -0700

    Pertemuan 2: Blade Tempalate

---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,4 @@
+<x-app>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h1 class="fw-bold">student page</h1>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/student', function () {
+    return view('student.index',['title' => 'Student']);
+});
+Route::get('/student/creat', function () {
+    return view('student.creat',['title' => 'Student']);
+});
